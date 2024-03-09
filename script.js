6bd0a2b2cf0280d79cdb95588a103dab757985f5
Pranav <pranavhariharan145@gmail.com>
2024-03-10 00:00:36 +0530

    made a counter Program ._.

--- script.js
+++ script.js
@@ -7,11 +7,40 @@ document.getElementById("pa").textContent = "I like burger"
 let uname  = prompt("What is your username: ")
 document.getElementById("main").textContent = `your name is ${uname}`
 */
-let username;
-// Select the button first
-document.getElementById("submit").onclick = function() {
-    username = document.getElementById("text").value
-    console.log(username)
-    document.getElementById("change").textContent = `Hello ${username}`
+// let username;
+// // Select the button first
+// document.getElementById("submit").onclick = function() {
+//     username = document.getElementById("text").value
+//     console.log(username)
+//     document.getElementById("change").textContent = `Hello ${username}`
+// }
+
+
+// document.getElementById("submit").onclick = function()
+// {
+//     radius = Number(document.getElementById("text").value)
+//     let cfer = 2*Math.PI*radius
+//     document.getElementById("ans").textContent = `Circumference is ${cfer}`
+// }
+
+// Counter Program
+let number = 0;
+
+document.getElementById("decrement").onclick = function()
+{
+    number --
+    document.getElementById("number").textContent = number
+}
+
+document.getElementById("reset").onclick = function()
+{
+    number = 0
+    document.getElementById("number").textContent = number
+}
+
+document.getElementById("increment").onclick = function()
+{
+    number ++
+    document.getElementById("number").textContent = number
 }
 
