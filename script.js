110abab9e4bc71c49121a82cd15bff9e482bc261
Pranav <pranavhariharan145@gmail.com>
2024-03-09 23:04:15 +0530

    User input using textbox

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+/*
 let text;
 text = "Ghost"
 let age = 20
-document.getElementById("main").textContent = `your name is ${text}`
-
 document.getElementById("pa").textContent = "I like burger"
+
+let uname  = prompt("What is your username: ")
+document.getElementById("main").textContent = `your name is ${uname}`
+*/
+let username;
+// Select the button first
+document.getElementById("submit").onclick = function() {
+    username = document.getElementById("text").value
+    console.log(username)
+    document.getElementById("change").textContent = `Hello ${username}`
+}
+
